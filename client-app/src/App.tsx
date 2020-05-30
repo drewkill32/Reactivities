824a5999217c8b9191e9bfdc1e74fdde182bbab7
Drew Killion <adk@killion.me>
2020-05-30 16:11:18 -0400

    adding client app

---
+++ client-app/src/App.tsx
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import { Header, Icon, List } from "semantic-ui-react";
+import "./App.css";
+import axios from "axios";
+
+class App extends Component {
+  state = {
+    values: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:5000/api/values").then((response) => {
+      this.setState({
+        values: response.data,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header as="h2" icon>
+          <Icon name="users" />
+          Users
+          <Header.Subheader>
+            Manage your account settings and set e-mail preferences.
+          </Header.Subheader>
+        </Header>
+        <List>
+          {this.state.values.map((value: any) => (
+            <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+      </div>
+    );
+  }
+}
+
+export default App;
